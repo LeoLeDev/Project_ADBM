d17d28545079b2fbba5c3a39a448e0bdfcdde131
SantiagoMartin2002 <150465646+SantiagoMartin2002@users.noreply.github.com>
2023-12-18 19:49:17 +0100

    Add files via upload

--- create_tables.sql
+++ create_tables.sql
@@ -2,42 +2,43 @@
 -- Script de création des tables  --
 /************************************/
 
-drop table vendor;
-drop table productvendor;
-drop table purchseorderheader;
 drop table purchaseorderdetail;
+drop table purchaseorderheader;
+drop table productvendor;
+drop table vendor;
+
 
 CREATE TABLE vendor(
 businessentityid INTEGER NOT NULL,
-accountnumber VARCHAR2(32),
-name VARCHAR2(32),
+accountnumber VARCHAR2(50),
+name VARCHAR2(50),
 creditrating INTEGER,
-preferredvendorstatus VARCHAR2(32),
-activeflag VARCHAR2(32),
-purchasingwebserviceurl VARCHAR(32),
+preferredvendorstatus VARCHAR2(50),
+activeflag VARCHAR2(50),
+purchasingwebserviceurl VARCHAR(50),
 modifieddate TIMESTAMP,
 CONSTRAINT pk_vendor PRIMARY KEY (businessentityid));
 
 CREATE TABLE productvendor(
 productid INTEGER NOT NULL,
 businessentityid INTEGER NOT NULL,
-averageleadtime INTEGER,
+averageleadtime NUMBER,
 standardprice DECIMAL(12,6),
 lastreceiptcost DECIMAL(12,6),
 lastreceiptdate TIMESTAMP,
-minorderqty INTEGER,
-maxorderqty INTEGER,
-onorderqty VARCHAR2(32),
-unitmeasurecode VARCHAR2(32),
+minorderqty NUMBER,
+maxorderqty NUMBER,
+onorderqty VARCHAR2(50),
+unitmeasurecode VARCHAR2(50),
 modifieddate TIMESTAMP,
-CONSTRAINT pk_productvendor PRIMARY KEY (productid, businessentityid),
-CONSTRAINT fk_productvendor FOREIGN KEY (businessentityid) REFERENCES vendor(businessentityid));
+CONSTRAINT fk_productvendor FOREIGN KEY (businessentityid) REFERENCES vendor(businessentityid),
+CONSTRAINT pk_productvendor PRIMARY KEY (productid, businessentityid));
 
 CREATE TABLE purchaseorderheader(
 purchaseorderid INTEGER NOT NULL,
 revisionnumber INTEGER,
 status INTEGER,
-employeeid INTEGER,
+employeeid NUMBER(5),
 vendorid INTEGER NOT NULL,
 shipmethodid INTEGER,
 orderdate TIMESTAMP,
@@ -49,16 +50,16 @@ modifieddate TIMESTAMP,
 CONSTRAINT pk_purchaseorderheader PRIMARY KEY (purchaseorderid),
 CONSTRAINT fk_purchaseorderheader FOREIGN KEY (vendorid) REFERENCES vendor(businessentityid));
 
-CREATE TABLE purchaseorderdetail(
+CREATE TABLE purchaseorderdetail
+(
 purchaseorderid INTEGER NOT NULL,
 purchaseorderdetailid INTEGER NOT NULL,
 duedate TIMESTAMP,
 orderqty NUMBER,
-productid INTEGER NOT NULL,
+productid NUMBER,
 unitprice DECIMAL(12,6),
-receivedqty DECIMAL(5,2),
-rejectedqty DECIMAL(5,2),
+receivedqty NUMBER,
+rejectedqty NUMBER,
 modifieddate TIMESTAMP,
-CONSTRAINT pk_purchaseorderdetail PRIMARY KEY (purchaseorderid, purchaseorderdetailid),
-CONSTRAINT fk_purchaseorderdetail1 FOREIGN KEY (purchaseorderid) REFERENCES purchaseorderheader(purchaseorderid),
-CONSTRAINT fk_purchaseorderdetail2 FOREIGN KEY (productid) REFERENCES productvendor(productid));
+CONSTRAINT fk_purchaseorderdetail FOREIGN KEY (purchaseorderid) REFERENCES purchaseorderheader(purchaseorderid),
+CONSTRAINT pk_purchaseorderdetail PRIMARY KEY (purchaseorderid, purchaseorderdetailid));
